26196fbc1902f32babf013efb351e22c820916f2
LeenEl-hijaa <leenkhaled77@gamil.com>
2022-07-02 02:36:46 +0300

    main

---
+++ movie/src/components/about.js
@@ -0,0 +1,12 @@
+function About() {
+
+
+    return(
+        <div className="container">
+            
+            <h3>About Us</h3>
+            IMDb is an online database of information related to films, television series, home videos, video games, and streaming content online – including cast, production crew and personal biographies, plot summaries, trivia, ratings, and fan and critical reviews      </div>
+    );
+}
+
+export default About;
